8fc562a43c9d9289640cbedb12662df8bbbe2d43
Mariam Nasser <mariamnasser979@gmail.com>
2020-03-02 23:27:11 +0200

    20170284
    Fibonacci

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	private static ISubscriber subscribers [] = {
 
 			new SphereArea(), new LucasSeries(),
-			new Spherevolume()
+			new Spherevolume(), new Fibonacci()
 
 	};
 	public static void main(String[] args) {
